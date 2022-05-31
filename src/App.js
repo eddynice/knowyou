64e2247e2c44a1b6a6eb2fda7d2afe76cd769cc4
Osaze <osareniyeosazee@gmail.com>
2022-05-31 23:12:04 +0100

    know u

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "./component/card/Box";
+import Heading from "./component/heading/Heading";
+import Search from "./component/search/Search";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+<Heading/>
+<Box/>
+<Search/>
+
+   </div>
   );
 }
 
---
+++ src/component/card/Box.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./box.css"
+
+export const Box = () => {
+  return (
+    <div className="box">
+        <p>box1wjjjjjjj</p>
+        <p>boxannn2</p>
+        <p>boxjssss3</p>
+        <p>boxjsjsjsj4</p>
+        <p>box5</p>
+        <p>box6</p>
+
+
+    </div>
+  )
+}
